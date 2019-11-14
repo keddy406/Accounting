e9a4830c774c17c6d904e7c2656e499eea2fa357
keddy406 <keddy406@hotmail.com>
2019-11-14 19:56:06 +0800

    add read file

--- Accounting.py
+++ Accounting.py
@@ -1,4 +1,13 @@
+#讀取檔案
 products = []
+with open('products.csv', 'r', encoding= 'utf-8') as f:
+	for line in f:
+		if '商品名稱,價格' in line :
+			continue
+		name, price = line.strip().split(',') #以,為切割標準
+		products.append([name, price])
+print(products)
+#讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -7,10 +16,11 @@ while True:
 	products.append([name, price])
 	price = int (price) #假設把價格改成整數
 print (products)
-
+#列出購買紀錄
 for p in products:
 	print(p[0],'的價格是',p[1])
-
+#寫入檔案
 with open('products.csv', 'w',encoding= 'utf-8') as f:  #encoding 改編碼成 utf-8
+	f.write('商品名稱,價格\n')
 	for p in products:
 		f.write(p[0]+',' + str(p[1]) +'\n')  #str改成字串 

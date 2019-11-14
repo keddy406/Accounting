f296d49f64c3f3290c45259471c7d8027418554d
keddy406 <keddy406@hotmail.com>
2019-11-14 20:09:08 +0800

    add exist

--- Accounting.py
+++ Accounting.py
@@ -1,12 +1,20 @@
-#讀取檔案
+import os # operation system
 products = []
-with open('products.csv', 'r', encoding= 'utf-8') as f:
-	for line in f:
-		if '商品名稱,價格' in line :
-			continue
-		name, price = line.strip().split(',') #以,為切割標準
-		products.append([name, price])
-print(products)
+if os.path.isfile('products.csv'):
+	print('Yeah!')
+	#讀取檔案
+	
+	with open('products.csv', 'r', ) as f:
+		for line in f:
+			if '商品名稱,價格' in line :
+				continue
+			name, price = line.strip().split(',') #以,為切割標準
+			products.append([name, price])
+	print(products)
+else:
+	print('Woops!')
+
+
 #讓使用者輸入
 while True:
 	name = input('請輸入商品名稱: ')
@@ -20,7 +28,7 @@ print (products)
 for p in products:
 	print(p[0],'的價格是',p[1])
 #寫入檔案
-with open('products.csv', 'w',encoding= 'utf-8') as f:  #encoding 改編碼成 utf-8
+with open('products.csv', 'w',) as f:  #encoding= 'utf-8'encoding 改編碼成 utf-8
 	f.write('商品名稱,價格\n')
 	for p in products:
 		f.write(p[0]+',' + str(p[1]) +'\n')  #str改成字串 

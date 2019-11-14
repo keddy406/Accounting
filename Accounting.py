ab35eed43b9f87c4772d785a8555dd6854f57590
keddy406 <keddy406@hotmail.com>
2019-11-14 19:33:56 +0800

    add header

--- Accounting.py
+++ Accounting.py
@@ -5,11 +5,12 @@ while True:
 		break
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
+	price = int (price) #假設把價格改成整數
 print (products)
 
 for p in products:
 	print(p[0],'的價格是',p[1])
 
-with open('products.txt', 'w') as f:
+with open('products.csv', 'w',encoding= 'utf-8') as f:  #encoding 改編碼成 utf-8
 	for p in products:
-		f.write(p[0]+',' + p[1] +'\n')  #字串相加
+		f.write(p[0]+',' + str(p[1]) +'\n')  #str改成字串 

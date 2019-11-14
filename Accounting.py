83f690a17c499e28ae75651de2f9b14752e6cc2b
keddy406 <keddy406@hotmail.com>
2019-11-14 19:23:16 +0800

    write

--- Accounting.py
+++ Accounting.py
@@ -6,3 +6,10 @@ while True:
 	price = input('請輸入商品價格: ')
 	products.append([name, price])
 print (products)
+
+for p in products:
+	print(p[0],'的價格是',p[1])
+
+with open('products.txt', 'w') as f:
+	for p in products:
+		f.write(p[0]+',' + p[1] +'\n')  #字串相加
